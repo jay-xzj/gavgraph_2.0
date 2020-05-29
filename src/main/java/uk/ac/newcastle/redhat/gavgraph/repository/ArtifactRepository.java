eabb4e13be23c851efdfe8bada677d4d7d941be8
Jay_Xu <xzj_thrive@126.com>
2020-05-29 09:50:48 +0100

    add more queries for all entities

--- src/main/java/uk/ac/newcastle/redhat/gavgraph/repository/ArtifactRepository.java
+++ src/main/java/uk/ac/newcastle/redhat/gavgraph/repository/ArtifactRepository.java
@@ -1,5 +1,6 @@
 package uk.ac.newcastle.redhat.gavgraph.repository;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import uk.ac.newcastle.redhat.gavgraph.domain.Artifact;
 
@@ -7,15 +8,15 @@ import java.util.List;
 
 public interface ArtifactRepository extends Neo4jRepository<Artifact, Long> {
 
-    //@Query("MATCH (a:Artifact) WHERE a.groupId = $groupId RETURN a")
+    @Query("MATCH (a:Artifact) WHERE a.groupId = $groupId RETURN a")
     List<Artifact> getAllByGroupId(String groupId);
 
-    //@Query("MATCH (a:Artifact) WHERE a.artifactId = $artifactId RETURN a")
+    @Query("MATCH (a:Artifact) WHERE a.artifactId = $artifactId RETURN a")
     List<Artifact> getAllByArtifactId(String artifactId);
 
-    //@Query("MATCH (a:Artifact) WHERE a.artifactId CONTAINS $artifactId RETURN a")
+    @Query("MATCH (a:Artifact) WHERE a.artifactId CONTAINS $artifactId RETURN a")
     List<Artifact> getAllByArtifactIdContains(String artifactId);
 
-    //@Query("MATCH (a:Artifact) WHERE a.groupId CONTAINS $groupId RETURN a")
+    @Query("MATCH (a:Artifact) WHERE a.groupId CONTAINS $groupId RETURN a")
     List<Artifact> getALlByGroupIdContains(String groupId);
 }
